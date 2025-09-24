9d3701bb1b65da850faf90f2d07980722d394c0c
HARIKRISHNA0609 <kmharikrishna4002@gmail.com>
2025-09-24 19:14:30 +0000

    summary datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,31 @@
+// primtive 
+
+// 7 types:  string, number, boolean, null, undefined, symbol, big int
+
+// js is dynamically typed language 
+
+
+//symbol declare karne ke liye
+
+//eg 
+const id = Symbol('123')
+
+const anotherId = ('123')
+
+console.log(id === anotherId)
+
+
+
+// reference type (non primitive)
+
+//types: Array, objects,functions
+
+const hero = ["shaktiman", "raj", "powerranger"]
+
+{
+name = harikrishna 
+age =  21
+
+}
+
+function(){}
