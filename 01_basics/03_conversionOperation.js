c56e5833ca81a50e5228ad44eddece0c3e9b8084
HARIKRISHNA0609 <kmharikrishna4002@gmail.com>
2025-09-24 17:02:55 +0000

    jsnewtopic22sept

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,3 +30,27 @@
  let stringNumber = String(someNumber)
  console.log( typeof stringNumber)
 
+
+ let value = 3
+let negValue = -value
+
+console.log(negValue)
+
+//console.log(2**2)
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2%5)
+//console.log(2/3)
+
+
+let str1 = "hello"
+let str2 = "Harkrishna"
+let str3 = str1 + str2
+console.log(str3)
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter)
+
+
+
