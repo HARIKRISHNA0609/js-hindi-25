cc012d6718d2b0a453b6b09e7792a101507c8016
HARIKRISHNA0609 <kmharikrishna4002@gmail.com>
2025-09-26 19:27:09 +0000

    strings in little detail

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,31 @@
+const name = "harikrishna"
+const repocount = 50
+
+// console.log(name + repocount + "anything u wnat to add value")  dont use it it is very much outdated rather use backticks
+
+
+console.log(`hello my name is ${name} and my repocount is ${repocount}` )
+
+
+const gameName = new String('harikrishna')
+console.log(gameName.length)
+
+
+console.log(gameName.charAt(2))
+
+console.log(gameName.indexOf('n'))
+
+
+const newStringOne = "   HARIKRISHNA"
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+
+const url = "https:/harikrishnasivaji06"
+
+console.log(url.replace('06','04'))
+
+
+console.log(url.includes('harikrishna'))
+
+console.log(url.includes('04'))
