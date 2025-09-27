ba0c78ad9a31238b63e39894fcc8155ae1292190
HARIKRISHNA0609 <kmharikrishna4002@gmail.com>
2025-09-27 19:30:01 +0000

    dateinJS

---
+++ 01_basics/07_dateinjs.js
@@ -0,0 +1,34 @@
+//let myDate = new Date()
+//console.log(myDate.toString())
+
+//console.log(myDate.toISOString())
+
+//console.log(myDate.toJSON())
+
+//console.log(myDate.toLocaleDateString())
+
+//console.log(myDate.toLocaleString())
+
+//console.log(typeof myDate)
+
+
+//let myCreatedate = new Date(2024,0,23)
+//let myCreatedDate = new Date("01-14-2023")
+//console.log(myCreatedDate.toDateString())
+
+
+//let myTimeStamp = Date.now()
+//console.log(myCreatedDate.getTime())
+
+
+
+let newdate = new Date()
+console.log(newdate)
+console.log(newdate.getMonth())
+
+
+
+newdate.toLocaleString('default', {
+    weekday:"long"
+    
+})
