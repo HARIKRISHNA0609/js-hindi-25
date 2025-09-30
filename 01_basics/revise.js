7dfbebd7c168c044198ecc07dede6d464c8d0747
HARIKRISHNA0609 <kmharikrishna4002@gmail.com>
2025-09-30 19:59:54 +0000

    revise

---
+++ 01_basics/revise.js
@@ -0,0 +1,63 @@
+//const accountId = 12345
+//let accountName = "Harikrishna"
+
+//accountCity = "mumbai"
+
+
+
+ //accountCity ="jaipur"
+
+ //console.table([accountId,accountName,accountCity])
+
+
+
+ //console.log(3+3)
+
+ //let myName = "harikrishna"
+ //const myAddress = "marol andheri east"
+
+ //let myNumber = 123456789
+
+ //console.log(myName,myAddress,myNumber);
+
+ //console.log(typeof myName )
+
+ //let landMark;
+ //console.log(landMark)
+
+//let score = 50
+
+
+//let score = "11"
+//console.log(typeof score)
+
+ 
+//let valueInNumber = Number(score)
+ //console.log(ValueInnumber)
+
+ //console.log(typeof valueInNumber) 
+
+ //let loginId = 1
+
+ //numberToBoolean = Boolean(loginId)
+ //console.log(typeof numberToBoolean)
+
+//let string1 =23
+//let string2 =42
+//string3 =string1+string2
+//console.log(string3)
+
+//const name ="harikrishna"
+//const persuaing ="be extc"
+
+//console.log(`hello my name is ${name} and i am currently persuaing ${persuaing}`);
+
+
+let myDate = Date()
+console.log(myDate);
+
+console.log(myDate.toString())
+
+
+let  myCreateddate = new Date(02-15-2000)
+console.log(myCreateddate)
