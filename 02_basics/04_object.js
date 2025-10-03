08b8d21eeaf4ab9dafedf771f52c88207f754dd6
HARIKRISHNA0609 <kmharikrishna4002@gmail.com>
2025-10-03 18:53:35 +0000

    object 2part

---
+++ 02_basics/04_object.js
@@ -0,0 +1,38 @@
+//const tinderUser = new Object()   //WAY  OF DECLARING OBJECT
+
+const tinderUser = {}  // ANOTHER WAY OF DECLARING OBJECT 
+
+tinderUser.id = "123abc"
+tinderUser.name = "hari"
+tinderUser.isLoggedIn =false
+
+
+//console.log(tinderUser)
+
+
+const regularUser = {
+
+    email: "some@gmail.com",
+
+  fullname: {
+
+    userfullname: {
+        firstname: "hari",
+        lastname: "km"
+    }
+}
+}
+
+//console.log(regularUser.fullname)
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 ={3:"a",4:"b"}
+
+//const obj3 ={obj1,obj2}
+
+//const obj3 = Object.assign({},obj1,obj2)
+
+
+const obj3 ={...obj1,...obj2}
+console.log(obj3)
